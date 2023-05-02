f5744c9ab5a3dffc7003d9b6dcf11dcc39c49bc7
jmarcos16 <contato.marcosrf@gmail.com>
2023-05-02 14:22:58 -0300

    TODO-3: create table component defualt for project

--- tests/Feature/Livewire/Todo/IndexTest.php
+++ tests/Feature/Livewire/Todo/IndexTest.php
@@ -7,7 +7,7 @@ it('should be able to see todos', function () {
     ]);
 
     $this->actingAs($user)
-        ->get(route('todo'))
+        ->get(route('todo.index'))
         ->assertSee($todos[0]->title)
         ->assertSee($todos[1]->title)
         ->assertSee($todos[2]->title);
