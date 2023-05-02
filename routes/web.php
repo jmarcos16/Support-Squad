9191b146e740febd69c8a3eaba8308956fd1a749
jmarcos16 <contato.marcosrf@gmail.com>
2023-05-02 11:47:41 -0300

    TODO-3: Config if app is local auto login with id number 1

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,9 @@
 use App\Http\Controllers\{ProfileController, TodoController};
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+if (app()->isLocal()) {
+    auth()->loginUsingId(1);
+}
 
 Route::get('/', function () {
     return view('welcome');
